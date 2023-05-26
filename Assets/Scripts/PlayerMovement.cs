5af82a031ec9cae916333dad9bf16674901385df
NotClassified <79024057+NotClassified@users.noreply.github.com>
2023-05-25 17:09:42 -0700

    Player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //[SerializeField] InputActionAsset controls;
+
+    [SerializeField] float forwardAcceleration;
+    //[SerializeField] float forwardMaxSpeed;
+
+    [SerializeField] float backwardAcceleration;
+    //[SerializeField] float backwardMaxSpeed;
+
+    [SerializeField] float turnAcceleration;
+    //[SerializeField] float turnMaxSpeed;
+
+    Rigidbody2D rb;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(forwardAcceleration * transform.up);
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(backwardAcceleration * -transform.up);
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddTorque(-turnAcceleration);
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddTorque(turnAcceleration);
+
+        }
     }
 }
