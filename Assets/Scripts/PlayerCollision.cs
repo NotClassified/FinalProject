7030b8bac600e5979bb77f3160b6ec3904e618ac
NotClassified <79024057+NotClassified@users.noreply.github.com>
2023-05-28 22:39:50 -0700

    Asteroids slowing down player

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Asteroid collidedAsteroid;
+
+    [SerializeField] float slowDownDuration;
+    [SerializeField] float dragIncrease;
+    float dragTimer;
+    Coroutine slowerSpeedRoutine;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Asteroid>(out collidedAsteroid))
+        {
+            if (slowerSpeedRoutine == null)
+            {
+                //don't have multiple SlowDownSpeed routines
+                slowerSpeedRoutine = StartCoroutine(SlowDownSpeed());
+            }
+            dragTimer = slowDownDuration;
+            collision.gameObject.SetActive(false);
+        }
+    }
+
+    IEnumerator SlowDownSpeed()
+    {
+        float initialDrag = rb.drag;
+        rb.drag += dragIncrease;
+        while (dragTimer > 0)
+        {
+            dragTimer -= Time.deltaTime;
+            yield return null;
+        }
+        rb.drag = initialDrag;
+        slowerSpeedRoutine = null;
+    }
+}
