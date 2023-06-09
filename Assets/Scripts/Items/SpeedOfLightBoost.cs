00c3c6dc5ebbfbea0f103a5eb54fb2ec04469c82
NotClassified <79024057+NotClassified@users.noreply.github.com>
2023-06-09 13:44:21 -0700

    speed of light boost change
    -player ends closer (after boost) to the rims of the track the slower the player moving (slower player = closer to track rims)

--- Assets/Scripts/Items/SpeedOfLightBoost.cs
+++ Assets/Scripts/Items/SpeedOfLightBoost.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class SpeedOfLightBoost : Item
 {
     [SerializeField] ContactFilter2D filter;
-    [SerializeField] float offsetOfTargetPos;
+    [SerializeField] float minOffsetOfTargetPos;
+    [SerializeField] float maxOffsetOfTargetPos;
+    float offsetOfTargetPos;
+    [SerializeField] float maxVelocity;
 
     public override void Use(GameObject player)
     {
@@ -13,6 +16,10 @@ public class SpeedOfLightBoost : Item
 
         RaycastHit2D[] results = new RaycastHit2D[1];
         Physics2D.Raycast(player.transform.position, player.transform.up, filter, results);
+
+
+        float playerVelocity = player.GetComponent<Rigidbody2D>().velocity.magnitude;
+        offsetOfTargetPos = Mathf.Lerp(minOffsetOfTargetPos, maxOffsetOfTargetPos, playerVelocity / maxVelocity);
 
         player.transform.position = results[0].point + offsetOfTargetPos * -(Vector2)player.transform.up;
     }
